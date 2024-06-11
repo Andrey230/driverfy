8fbe2b2fe7780201186fd73a04c602c8673d5e96
a.bilous <a.bilous@softum.io>
2024-06-11 14:53:37 +0200

    update upload ddd file

--- src/Service/DriverParser.php
+++ src/Service/DriverParser.php
@@ -4,7 +4,7 @@ namespace App\Service;
 
 class DriverParser implements ParserInterface
 {
-    const SCRIPT_PATH = '../src/Fixtures/dddxml.centos7';
+    const SCRIPT_PATH = '../src/Fixtures/dddxml.ubuntu';
 
     public function parse(string $file): array
     {
